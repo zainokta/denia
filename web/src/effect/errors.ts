eeae526d6787a84d5b45ce12d7f156090f2ba406
zainokta <zainokta@gmail.com>
2026-05-25 13:47:19 +0700

    feat(web): auth schema + ApiClient methods

--- web/src/effect/errors.ts
+++ web/src/effect/errors.ts
@@ -2,6 +2,7 @@ import { Schema } from 'effect'
 
 export class ApiError extends Schema.TaggedErrorClass<ApiError>()('ApiError', {
   message: Schema.String,
+  status: Schema.Number,
 }) {}
 
 export class DecodeError extends Schema.TaggedErrorClass<DecodeError>()(
